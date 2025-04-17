634704d53e9c17cbb263cd19f4afb0812457eab4
Afonin Alexei <aleksafonin2@mail.ru>
2025-04-17 07:34:25 +0300

    Add files via upload

---
+++ backend/api-gateway/src/main/java/ru/alafonin4/apigateway/ApiGatewayApplication.java
@@ -0,0 +1,12 @@
+package ru.alafonin4.apigateway;
+
+import jdk.jfr.Enabled;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGatewayApplication.class, args);
+    }
+}
